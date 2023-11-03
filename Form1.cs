5d7227c1fc34343a5f98ba3d77bbaad8d171d4b8
Alvin Alamgir berglund <alvinalamgirberglund@gmail.com>
2023-11-03 12:05:32 +0100

    created new files

--- Form1.cs
+++ Form1.cs
@@ -5,14 +5,17 @@ namespace RecipeGenius
 {
     public partial class Form1 : Form
     {
-        private List<string[]> userDataList;
+       
 
         RecipeBook recipeBook = new RecipeBook();
-        public static string? titel { get; set; }
+        private List<string[]> userDataList;
+        private UserDataManager userDataManager;
+
         public Form1()
         {
             InitializeComponent();
-            userDataList = recipeBook.LoadRecipiesData();
+            userDataManager = new UserDataManager();
+            userDataList = userDataManager.GetUserDataList();
             PopulateListBox();
         }
 
@@ -49,68 +52,21 @@ namespace RecipeGenius
             string enteredUserName = UserName.Text;
             string enteredPassword = UserPassword.Text;
 
-            // Check if either of the textboxes is empty.
-            if (string.IsNullOrEmpty(enteredUserName) || string.IsNullOrEmpty(enteredPassword))
+            
+
+            if (userDataManager.CheckUserCredentials(enteredUserName, enteredPassword))
             {
-                // Display an error message if either field is empty.
-                Reg.Text = "Please enter both username and password.";
+                AdminNewForm adminNewForm = new AdminNewForm();
+                adminNewForm.Show();
+                this.Hide();
             }
             else
             {
-
-                // Call the CheckCredentials method to verify the entered username and password.
-                if (CheckCredentials(enteredUserName, enteredPassword))
-                {
-                    // If the credentials are correct, create a new instance of Form3 and show it.
-                    AdminNewForm adminNewForm = new AdminNewForm();
-                    adminNewForm.Show();
-                    // Hide the current form (Form1).
-                    this.Hide();
-                }
-                else
-                {
-                    // If the credentials are incorrect, display an error message in the "Reg" label.
-                    Reg.Text = "Incorrect username or password. Please try again.";
-                }
+                Reg.Text = "Incorrect username or password. Please try again.";
             }
 
         }
-        private bool CheckCredentials(string enteredUserName, string enteredPassword)
-        {
-            // Specify the path to the file that stores the admin credentials.
-            string filePath = recipeBook.AdminLoggIn;
-
-            try
-            {
-                // Use a StreamReader to read the file line by line.
-                using (StreamReader reader = new StreamReader(filePath))
-                {
-                    string line;
-
-                    // Iterate through each line in the file.
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        // Split the line into parts based on the ',' delimiter.
-                        string[] parts = line.Split(',');
-
-                        // Check if the line has exactly 2 parts (username and password) and they match the entered credentials.
-                        if (parts.Length == 2 && parts[0] == enteredUserName && parts[1] == enteredPassword)
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                // If an error occurs while reading the file, display an error message in the "Reg" label.
-                Reg.Text = ($"An error occurred: {ex.Message}");
-                recipeBook.LogErrorToFile(ex.Message);
-            }
-            // If no valid credentials are found or an error occurred, return false.
-            return false;
-        }
-
+        
         private void aboutUsOpen_Click(object sender, EventArgs e)
         {
             AboutUs aboutUs = new AboutUs();
---
+++ UserDataManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RecipeGenius
+{
+    public class UserDataManager
+    {
+        private List<string[]> userDataList;
+
+        public UserDataManager()
+        {
+            userDataList = LoadUserData();
+        }
+
+        public List<string[]> GetUserDataList()
+        {
+            return userDataList;
+        }
+
+        public bool CheckUserCredentials(string enteredUserName, string enteredPassword)
+        {
+            foreach (string[] user in userDataList)
+            {
+                if (user.Length == 2 && user[0] == enteredUserName && user[1] == enteredPassword)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private List<string[]> LoadUserData()
+        {
+            List<string[]> data = new List<string[]>();
+
+            try
+            {
+                string filePath = "C:\\Visual studio projekt\\Windowsforms projekt\\Onsdag-Fredag\\RecipeGenius\\RecipeGenius\\Admin.txt";
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Length == 2)
+                        {
+                            data.Add(parts);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+         
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return data;
+        }
+    }
+}
